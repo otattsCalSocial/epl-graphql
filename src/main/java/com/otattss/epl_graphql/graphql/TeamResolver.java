9af638f6888314888f93b5a2d73962fe94f01ea8
Oliver Tattersall <oliver@cal.social>
2025-09-13 03:13:15 +0100

    Add single team query by ID

--- src/main/java/com/otattss/epl_graphql/graphql/TeamResolver.java
+++ src/main/java/com/otattss/epl_graphql/graphql/TeamResolver.java
@@ -2,6 +2,7 @@ package com.otattss.epl_graphql.graphql;
 
 import com.otattss.epl_graphql.domain.Team;
 import com.otattss.epl_graphql.repo.TeamRepository;
+import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
@@ -19,5 +20,11 @@ public class TeamResolver {
     @QueryMapping
     public List<Team> teams() {
         return teamRepository.findAll();
+    }
+
+    @QueryMapping
+    public Team team(@Argument Long id){
+        return teamRepository.findById(id)
+            .orElseThrow(() -> new IllegalArgumentException("Team not found: " + id));
     }
 }
